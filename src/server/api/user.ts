988fe15c93e3e8dbe64a94e09e825f806f3005e3
Shawn Timothy Ike Barza <92981591+Pistachio21@users.noreply.github.com>
2024-05-13 18:28:47 +0800

    backend - added login

--- src/server/api/user.ts
+++ src/server/api/user.ts
@@ -1,23 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import bcrypt from 'bcrypt'
 
 dotenv.config();
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT: number = Number(process.env.PORT)!;
+const PORT = Number(process.env.PORT);
 const DATABASE_URL: string = process.env.DATABASE_URL!;
 
 app.use(express.json());
 
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.get("/api/users", async (_req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
+    
     res.json(users);
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//login backend for users
+app.get('/login', async (req, res) => {
+  const { email, password }: {email: string, password: string} = req.body;
+  try {
+    // Find a user by email
+    const user = await prisma.user.findUnique({
+      where: { 
+        email: email,
+        password: password
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Incorrect password' });
+    }
+
+    // Return successful login response
+    return res.status(200).json({
+      user: { email: user.email },
+    });
+  } catch (error: any) {
+    console.error('Error logging in:', error.message);
+    res.status(500).json({ error: 'An error occurred while logging in' });
   }
 });
 

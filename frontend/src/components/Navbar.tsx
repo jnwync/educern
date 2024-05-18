6b4db7dbdb10fb142317e53df437cceda92743d7
Jokkeeerrz <jpp200214@gmail.com>
2024-05-19 01:14:46 +0800

    add navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="w-full bg-stone-700 p-4 flex items-center justify between">
+      <div className="text-white text-2xl font-bold">EDUCERN</div>
+    </div>
+  )
+}
+
+export default Navbar;
